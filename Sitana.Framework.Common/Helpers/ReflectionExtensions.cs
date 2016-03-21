de2143b61db4e7c917115b02ade9ac8db13a3d8a
statica_kb <broczkowski@statica.pl>
2016-03-21 11:52:10 +0100

    Windows Phone extensions.

--- Sitana.Framework.Common/Helpers/ReflectionExtensions.cs
+++ Sitana.Framework.Common/Helpers/ReflectionExtensions.cs
@@ -18,99 +18,29 @@ namespace Sitana.Framework
 
     public static class ReflectionExtensions
     {
-        static Dictionary<Type, MethodInfo[]> _methods = new Dictionary<Type, MethodInfo[]>();
-        static Dictionary<Type, PropertyInfo[]> _properties = new Dictionary<Type, PropertyInfo[]>();
-
         public static FieldInfo GetField(this Type type, string name)
         {
-            Type baseType = type;
-            FieldInfo info = type.GetTypeInfo().GetDeclaredField(name);
-            while(info == null)
-            {
-                baseType = baseType.GetTypeInfo().BaseType;
-
-                if(baseType == null || baseType.GetTypeInfo().IsPrimitive)
-                {
-                    return null;
-                }
-
-                info = baseType.GetTypeInfo().GetDeclaredField(name);
-            }
-
-            return info;
+            return type.GetRuntimeField(name);
         }
 
         public static PropertyInfo GetProperty(this Type type, string name)
         {
-            Type baseType = type;
-            PropertyInfo info = type.GetTypeInfo().GetDeclaredProperty(name);
-            while (info == null)
-            {
-                baseType = baseType.GetTypeInfo().BaseType;
-
-                if (baseType == null || baseType.GetTypeInfo().IsPrimitive)
-                {
-                    return null;
-                }
-
-                info = baseType.GetTypeInfo().GetDeclaredProperty(name);
-            }
-
-            return info;
+            return type.GetRuntimeProperty(name);
         }
 
-        public static PropertyInfo[] GetProperties(this Type type)
+        public static IEnumerable<PropertyInfo> GetProperties(this Type type)
         {
-            PropertyInfo[] properties;
-            if (!_properties.TryGetValue(type, out properties))
-            {
-                List<PropertyInfo> list = new List<PropertyInfo>();
-
-                Type baseType = type;
-
-                for (; ; )
-                {
-                    list.AddRange(baseType.GetTypeInfo().DeclaredProperties);
-
-                    baseType = baseType.GetTypeInfo().BaseType;
-
-                    if (baseType == null || baseType.GetTypeInfo().IsPrimitive)
-                    {
-                        break;
-                    }
-                }
-
-                properties = list.ToArray();
-                _properties.Add(type, properties);
-            }
-            return properties;
+            return type.GetRuntimeProperties();
         }
 
-        public static MethodInfo[] GetMethods(this Type type)
+        public static IEnumerable<FieldInfo> GetFields(this Type type)
         {
-            MethodInfo[] methods;
-            if(!_methods.TryGetValue(type, out methods))
-            {
-                List<MethodInfo> list = new List<MethodInfo>();
+            return type.GetRuntimeFields();
+        }
 
-                Type baseType = type;
-
-                for (;;)
-                {
-                    list.AddRange(baseType.GetTypeInfo().DeclaredMethods);
-
-                    baseType = baseType.GetTypeInfo().BaseType;
-
-                    if (baseType == null || baseType.GetTypeInfo().IsPrimitive)
-                    {
-                        break;
-                    }
-                }
-
-                methods = list.ToArray();
-                _methods.Add(type, methods);
-            }
-            return methods;
+        public static IEnumerable<MethodInfo> GetMethods(this Type type)
+        {
+            return type.GetRuntimeMethods();
         }
     }
 }
